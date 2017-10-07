ad30ddb1c4381df62502b5f6b532e38afdee7fdf
Hang Zhao <zhaohang0124@gmail.com>
2017-10-06 21:46:44 -0400

    dataset download

---
+++ download_ADE20K.sh
@@ -0,0 +1,4 @@
+wget -O ./data/ADEChallengeData2016.zip http://data.csail.mit.edu/places/ADEchallenge/ADEChallengeData2016.zip
+unzip ./data/ADEChallengeData2016.zip -d ./data
+rm ./data/ADEChallengeData2016.zip
+echo "Dataset downloaded."
