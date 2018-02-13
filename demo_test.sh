bd181b74187c4bb01ad013629d1cef264133c9ca
Hang Zhao <zhaohang0124@gmail.com>
2018-02-13 18:33:35 -0500

    fix test demo

--- demo_test.sh
+++ demo_test.sh
@@ -1,10 +1,10 @@
 MODEL_PATH=./baseline-resnet34_dilated8-psp_bilinear
+RESULT_PATH=./
 mkdir $MODEL_PATH
 
 wget -P $MODEL_PATH http://sceneparsing.csail.mit.edu/model/pytorch/baseline-resnet34_dilated8-psp_bilinear/encoder_best.pth
 wget -P $MODEL_PATH http://sceneparsing.csail.mit.edu/model/pytorch/baseline-resnet34_dilated8-psp_bilinear/decoder_best.pth
-
-wget http://sceneparsing.csail.mit.edu//data/ADEChallengeData2016/images/validation/ADE_val_00000278.jpg
+wget -P $RESULT_PATH http://sceneparsing.csail.mit.edu//data/ADEChallengeData2016/images/validation/ADE_val_00000278.jpg
 
 python -u test.py \
   --model_path $MODEL_PATH \
@@ -12,4 +12,4 @@ python -u test.py \
   --arch_encoder resnet34_dilated8 \
   --arch_decoder psp_bilinear \
   --fc_dim 512 \
-  --result .
+  --result $RESULT_PATH
