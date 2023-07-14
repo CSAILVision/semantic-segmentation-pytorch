b9f15e6f1a9acd640beb21bcaaccf04afa493123
pythoscorpion <48407814+pythoscorpion@users.noreply.github.com>
2023-07-14 06:26:50 +0330

    Update th.py to works with python 3.10

--- mit_semseg/lib/utils/th.py
+++ mit_semseg/lib/utils/th.py
@@ -8,17 +8,17 @@ __all__ = ['as_variable', 'as_numpy', 'mark_volatile']
 def as_variable(obj):
     if isinstance(obj, Variable):
         return obj
-    if isinstance(obj, collections.Sequence):
+    if isinstance(obj, collections.abc.Sequence):
         return [as_variable(v) for v in obj]
-    elif isinstance(obj, collections.Mapping):
+    elif isinstance(obj, collections.abc.Mapping):
         return {k: as_variable(v) for k, v in obj.items()}
     else:
         return Variable(obj)
 
 def as_numpy(obj):
-    if isinstance(obj, collections.Sequence):
+    if isinstance(obj, collections.abc.Sequence):
         return [as_numpy(v) for v in obj]
-    elif isinstance(obj, collections.Mapping):
+    elif isinstance(obj, collections.abc.Mapping):
         return {k: as_numpy(v) for k, v in obj.items()}
     elif isinstance(obj, Variable):
         return obj.data.cpu().numpy()
@@ -33,9 +33,9 @@ def mark_volatile(obj):
     if isinstance(obj, Variable):
         obj.no_grad = True
         return obj
-    elif isinstance(obj, collections.Mapping):
+    elif isinstance(obj, collections.abc.Mapping):
         return {k: mark_volatile(o) for k, o in obj.items()}
-    elif isinstance(obj, collections.Sequence):
+    elif isinstance(obj, collections.abc.Sequence):
         return [mark_volatile(o) for o in obj]
     else:
         return obj
